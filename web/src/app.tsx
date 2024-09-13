fc2f174ebc5a202f09a08ce74264c5b4c9a835fb
Vinicius <beneditoloura@gmail.com>
2024-09-13 07:25:18 -0300

    refactor: change html button to component button

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,7 @@
 import { Plus } from 'lucide-react'
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start-illustration.svg'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -12,13 +13,10 @@ export function App() {
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
 
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-small font-medium tracking-tight hover:bg-violet-600"
-      >
+      <Button>
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
