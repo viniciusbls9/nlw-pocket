930dd0b711875112e60ecb3d917f3e0a3b9c3d84
Vinicius <beneditoloura@gmail.com>
2024-09-13 06:24:32 -0300

    refactor: separate project by paths

---
+++ server/src/http/routes/index.ts
@@ -0,0 +1,11 @@
+import { createGoalRoute } from './create-goal'
+import { createCompletionRoute } from './create-completion'
+import { getPendingGoalsRoute } from './get-pending-goals'
+import { getWeekSummaryRoute } from './get-week-summary'
+
+export {
+  createGoalRoute,
+  createCompletionRoute,
+  getPendingGoalsRoute,
+  getWeekSummaryRoute,
+}
