e2b14e0ec217647d1063b95bfddbad1a042dc56d
Vinicius <beneditoloura@gmail.com>
2024-09-13 07:43:04 -0300

    feat: create input

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -10,6 +10,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from './components/ui/dialog'
+import { Label } from './components/ui/label'
+import { Input } from './components/ui/input'
+import { RadioGroup, RadioGroupIndicator, RadioGroupItem } from './components/ui/radio-group'
 
 export function App() {
   return (
@@ -32,18 +35,52 @@ export function App() {
       </div>
 
       <DialogContent>
-        <div className="flex flex-col gap-3">
-          <div className="flex items-center justify-between">
-            <DialogTitle>Cadastrar meta</DialogTitle>
-            <DialogClose>
-              <X className="size-5 text-zinc-600" />
-            </DialogClose>
+        <div className="flex flex-col gap-6 h-full">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <DialogTitle>Cadastrar meta</DialogTitle>
+              <DialogClose>
+                <X className="size-5 text-zinc-600" />
+              </DialogClose>
+            </div>
+
+            <DialogDescription>
+              Adicione atividades que te fazem bem e que você quer continuar
+              praticando toda semana.
+            </DialogDescription>
           </div>
 
-          <DialogDescription>
-            Adicione atividades que te fazem bem e que você quer continuar
-            praticando toda semana.
-          </DialogDescription>
+          <form className="flex-1 flex flex-col justify-between">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="title">Qual a atividade?</Label>
+                <Input
+                  id="title"
+                  autoFocus
+                  placeholder="Praticar exercícios, meditar, etc..."
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="title">Quantas vezes na semana?</Label>
+                <RadioGroup>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    1x na semana
+                    <span>🥱</span>
+                  </RadioGroupItem>
+                </RadioGroup>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <DialogClose asChild>
+                <Button type="button" className="flex-1" variant="secondary">
+                  Fechar
+                </Button>
+              </DialogClose>
+              <Button className="flex-1">Salvar</Button>
+            </div>
+          </form>
         </div>
       </DialogContent>
     </Dialog>
