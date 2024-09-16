3bfafe8f92b7e022fcd791c117cb695d20016510
Vinicius <beneditoloura@gmail.com>
2024-09-16 14:16:08 -0300

    refactor: change interface values

--- web/src/http/get-summary.ts
+++ web/src/http/get-summary.ts
@@ -1,11 +1,14 @@
 export type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: {
-    id: string
-    title: string
-    completedAt: string
-  }[]
+    completed: number
+    total: number
+    goalsPerDay: Record<
+      string,
+      {
+        id: string
+        title: string
+        completedAt: string
+      }[]
+    >
 }
 
 export async function getSummary(): Promise<SummaryResponse> {
