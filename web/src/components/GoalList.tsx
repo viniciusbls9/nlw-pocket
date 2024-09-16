9c5bac816c461f445d350a8b47d863ff7607528e
Vinicius <beneditoloura@gmail.com>
2024-09-16 14:15:45 -0300

    refactor: change mocked data to api data

--- web/src/components/GoalList.tsx
+++ web/src/components/GoalList.tsx
@@ -1,15 +1,31 @@
+import dayjs from 'dayjs'
 import { CheckCircle2 } from 'lucide-react'
 
-export function GoalList() {
+interface GoalListProps {
+  goals: {
+    id: string
+    title: string
+    completedAt: string
+  }[]
+}
+
+export function GoalList({ goals }: GoalListProps) {
   return (
     <ul className="flex flex-col gap-3">
-      <li className="flex items-center gap-2">
-        <CheckCircle2 className="size-4 text-pink-500" />
-        <span className="text-sm text-zinc-400">
-          Você completou "<span className="text-zinc-100">Acordar cedo</span>"
-          às <span className="text-zinc-100">08:13h</span>
-        </span>
-      </li>
+      {goals.map(goal => {
+        const time = dayjs(goal.completedAt).format('HH:mm')
+
+        return (
+          <li key={goal.id} className="flex items-center gap-2">
+            <CheckCircle2 className="size-4 text-pink-500" />
+            <span className="text-sm text-zinc-400">
+              Você completou "
+              <span className="text-zinc-100">{goal.title}</span>" às{' '}
+              <span className="text-zinc-100">{time}h</span>
+            </span>
+          </li>
+        )
+      })}
     </ul>
   )
 }
