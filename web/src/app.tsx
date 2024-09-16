10b022707996e8742f077df7660f42782dfb0458
Vinicius <beneditoloura@gmail.com>
2024-09-16 06:53:27 -0300

    feat: using react query to get summary response

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,11 +1,34 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: {
+    id: string
+    title: string
+    completedAt: string
+  }[]
+}
 
 export function App() {
+  const { data } = useQuery<SummaryResponse>({
+    queryKey: ['summary'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/summary')
+      const data = await response.json()
+
+      return data.summary
+    },
+  })
+
   return (
     <Dialog>
-      <EmptyGoals />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
       <CreateGoal />
     </Dialog>
   )
