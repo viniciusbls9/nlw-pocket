4936be2dd1d2a8c161bbc44059fcfb508ae01946
Vinicius <beneditoloura@gmail.com>
2024-09-16 14:15:38 -0300

    refactor: pass API data to components

--- web/src/components/GoalContainer.tsx
+++ web/src/components/GoalContainer.tsx
@@ -1,16 +1,31 @@
+import dayjs from 'dayjs'
 import { DayOffWeek } from './DayOfWeek'
 import { GoalList } from './GoalList'
+import { SummaryResponse } from '../http/get-summary'
 
-export function GoalContainer() {
+interface GoalContainerProps {
+  goalsPerDay: SummaryResponse['goalsPerDay']
+}
+
+export function GoalContainer({
+  goalsPerDay,
+}: GoalContainerProps) {
   return (
     <div className="flex flex-col gap-6">
       <h2 className="text-xl font-medium">Sua semana</h2>
 
-      <div className="flex flex-col gap-4">
-        <DayOffWeek />
+      {Object.entries(goalsPerDay).map(([date, goals]) => {
+        const weekDay = dayjs(date).format('dddd')
+        const formattedDate = dayjs(date).format('D[ de ]MMMM')
 
-        <GoalList />
-      </div>
+        return (
+          <div key={date} className="flex flex-col gap-4">
+            <DayOffWeek weekDay={weekDay} date={formattedDate} />
+
+            <GoalList goals={goals} />
+          </div>
+        )
+      })}
     </div>
   )
 }
