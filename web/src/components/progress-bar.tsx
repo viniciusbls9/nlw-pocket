de0ffd30c6981aceed89b6370d2637f0e03645e3
Vinicius <beneditoloura@gmail.com>
2024-09-16 07:44:59 -0300

    feat: using API data on progress bar component

--- web/src/components/progress-bar.tsx
+++ web/src/components/progress-bar.tsx
@@ -1,18 +1,28 @@
 import { Progress, ProgressIndicator } from './ui/progress-bar'
 
-export function ProgressBar() {
+interface ProgressProps {
+  completed?: number
+  total?: number
+  completedPercentage: number
+}
+
+export function ProgressBar({
+  completed,
+  total,
+  completedPercentage,
+}: ProgressProps) {
   return (
     <div className="flex flex-col gap-3">
       <Progress value={8} max={15}>
-        <ProgressIndicator style={{ width: '50%' }} />
+        <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
       </Progress>
 
       <div className="flex items-center justify-between text-xs text-zinc-400">
         <span>
-          Você completou <span className="text-zinc-100">8</span> de{' '}
-          <span className="text-zinc-100">15</span> metas nessa semana.
+          Você completou <span className="text-zinc-100">{completed}</span> de{' '}
+          <span className="text-zinc-100">{total}</span> metas nessa semana.
         </span>
-        <span>50%</span>
+        <span>{completedPercentage}%</span>
       </div>
     </div>
   )
