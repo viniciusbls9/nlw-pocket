1be0c805e564e03ff040a2642b74be36ba2e1849
Vinicius <beneditoloura@gmail.com>
2024-09-17 06:08:17 -0300

    refactor: group query by date

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -1,4 +1,4 @@
-import { and, eq, gte, lte, sql } from 'drizzle-orm'
+import { and, desc, eq, gte, lte, sql } from 'drizzle-orm'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 import dayjs from 'dayjs'
@@ -57,6 +57,7 @@ export async function getWeekSummary() {
       })
       .from(goalsCompletedInWeek)
       .groupBy(goalsCompletedInWeek.completedAtDate)
+      .orderBy(desc(goalsCompletedInWeek.completedAtDate))
   )
 
   const result = await db
