bc1fe7758fb98ebc0e8ac3092ab1e942a0360035
Vinicius <beneditoloura@gmail.com>
2024-09-17 06:38:49 -0300

    refactor: add orderBy

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -39,6 +39,7 @@ export async function getWeekSummary() {
           lte(goalCompletions.createdAt, lastDayOfWeek)
         )
       )
+      .orderBy(desc(goalCompletions.createdAt))
   )
 
   const goalsCompletedByWeekDay = db.$with('goals_completed_by_week_day').as(
