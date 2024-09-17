a74701e71dd85e64bc90e5d05b2eda61d355a922
Vinicius <beneditoloura@gmail.com>
2024-09-17 06:08:35 -0300

    refactor: add api data on pending goals component

--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -1,13 +1,44 @@
 import { Plus } from 'lucide-react'
 import { OutlineButton } from './ui/outline-button'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { getPendingGoals } from '../http/get-pending-goals'
+import { createGoalCompletion } from '../http/create-goal-completion'
+import { invalidateQuery } from '../utils/invalidate-query'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
+
+  const { data } = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals,
+    staleTime: 1000 * 600, // 60 seconds
+  })
+
+  if (!data) {
+    return null
+  }
+
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+
+    invalidateQuery({ queryClient, queryId: 'summary' })
+    invalidateQuery({ queryClient, queryId: 'pending-goals' })
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
-      <OutlineButton>
-        <Plus className="size-4 text-zinc-600" />
-        Meditar
-      </OutlineButton>
+      {data.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompleteGoal(goal.id)}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
     </div>
   )
 }
