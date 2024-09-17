35047d6edff83113e52f5514e06cfba25ae5b3f3
Vinicius <beneditoloura@gmail.com>
2024-09-17 06:08:54 -0300

    refactor: adjust file

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -24,7 +24,6 @@ export function Summary({ summary }: SummaryProps) {
     (summary?.completed * 100) / summary?.total
   )
 
-
   return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
       <div className="flex items-center justify-between">
